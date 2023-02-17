d7d6a384c1be1a5d58280351c9c4b7a908668ccf
abhishek.paul0705@gmail.com <your_email_id>
2023-02-17 13:57:02 -0500

    added the user class

---
+++ Objects/user.py
@@ -0,0 +1,14 @@
+class user:
+    def __init__(self, userId, userName, userPrivledge) -> None:
+        self.userId = userId
+        self.userName = userName
+        self.userPrivledge = userPrivledge
+
+
+    
+
+    
+
+
+
+    
