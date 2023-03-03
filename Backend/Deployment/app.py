f67d11dc38259a2c731fe309856cae3859409d7e
abhishek.paul0705@gmail.com <your_email_id>
2023-03-03 13:39:32 -0500

    .

--- Backend/Deployment/app.py
+++ Backend/Deployment/app.py
@@ -1,11 +1,9 @@
+# move into app.py directory using cd and run app.py flask run in the terminal
+
 from flask import Flask, render_template
 import os
-
-
-
 # template = os.path.abspath('~/public/Frontend/templates')
 app = Flask(__name__)
-
 
 @app.route('/')
 def home_page():
@@ -13,7 +11,7 @@ def home_page():
 
 
 if __name__ == "main":
-    app.run()
+    app.run(host='0.0.0.0', port=30006, debug=True)
 
 
 
