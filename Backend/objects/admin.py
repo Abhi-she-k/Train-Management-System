72d0dad2c0b67c874e25c56fdadcc3765ed5561b
Abhishek Paul <87787307+Abhi-she-k@users.noreply.github.com>
2023-03-20 20:08:30 +0000

    .

--- Backend/objects/admin.py
+++ Backend/objects/admin.py
@@ -1,2 +1,31 @@
 class admin(user):
-    pass
+    def __init__(self, firstName, lastName, id, userName, password, email):
+      self.firstName = firstName
+      self.lastName = lastName
+      self.id = id
+      self.userName = userName
+      self.password = password
+      self.email = email 
+
+
+    def register(self):
+      pass
+
+
+    def login(self, userName, password):
+      pass
+
+    def authenticate(self):
+      pass
+
+    def forgotCredentials(self):
+      pass
+
+    def logout(self):
+      pass
+
+    def changeUsername(self):
+      pass
+
+    def changePassword(self):
+      pass
---
+++ Backend/objects/trainSchedule.py
@@ -0,0 +1,7 @@
+class trainSchedule():
+
+    def __init__(self, id, train_id) -> None:
+        
+        self.id = id
+        self.route_id = route_id
+        self.location_info = {}
