b446ad3a476eff9c222cef918d87ce4ce128fc1f
Abhishek Paul <abhishek.paul0705@gmail.com>
2023-03-24 21:28:14 -0400

    a

--- Deployment/objects/station.py
+++ Deployment/objects/station.py
@@ -2,5 +2,8 @@ class station():
     def __init__(self, id, name) -> None:
       self.id = id
       self.name = name
+
+    def __str__(self):
+      return self.name 
       
       
--- Deployment/objects/test.py
+++ Deployment/objects/test.py
@@ -2,9 +2,10 @@ from route import *
 from station import *
 from train import *
 from train import *
-from user import 
+from user import *
 
 
-stat1 = station("stat1")
+stat1 = station(1,"Bloor-Yonge")
+print(stat1)
 
 
