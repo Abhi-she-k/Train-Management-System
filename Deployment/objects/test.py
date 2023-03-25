fad0716298c49c7748551d2ebdd414b343a01506
Abhishek Paul <87787307+Abhi-she-k@users.noreply.github.com>
2023-03-25 17:58:50 +0000

    gdfb

--- Deployment/objects/test.py
+++ Deployment/objects/test.py
@@ -5,18 +5,15 @@ from train import *
 from user import *
 from trainSchedule import *
 
-stations1 = ["Kipling", "Islington", "Royal York", "Old Mill", "Jane", "Runnymede", "High Park", 
-                 "Keele", "Dundas West"]
-stations2 = ["Christie", "Bathurst", "Spadina", "St. George", "Bay", "Bloor-Yonge", 
-             "Sherbourne", "Castle Frank", "Broadview" ]
-stations3 = ["Chester", "Pape", "Donlands", "Greenwood", "Coxwell", "Woodbine", "Main Street", 
-                 "Victoria Park", "Warden"]
+stations1 = ["Kipling", "Islington", "Royal York", "Old Mill", "Jane", "Runnymede", "HighPark", "Keele", "Dundas West"]
+stations2 = ["Christie", "Bathurst", "Spadina", "St. George", "Bay", "Bloor-Yonge", "Sherbourne", "Castle Frank", "Broadview" ]
+stations3 = ["Chester", "Pape", "Donlands", "Greenwood", "Coxwell", "Woodbine", "Main Street", "Victoria Park", "Warden"]
 
 route1 = route()
 route2 = route()
 route3 = route()
 
-for i in range(9):
+for i in range(len(stations1)):
     stat1 = station(i, stations1[i])
     stat2 = station(i, stations2[i])
     stat3 = station(i, stations3[i])
@@ -24,9 +21,9 @@ for i in range(9):
     route2.addStation(stat2)
     route3.addStation(stat3)
 
-train1 = train(1, "line1Train", route1)
-train2 = train(2, "line2Train", route2)
-train3 = train(3, "line3Train", route3)
+train1 = train(1, "line1Train", route1, [2,3,4,2,2,1,2,6])
+train2 = train(2, "line2Train", route2, [2,3,4,2,2,1,2,6])
+train3 = train(3, "line3Train", route3, [2,3,4,2,2,1,2,6])
 
 mySchedule = trainSchedule(1)
 mySchedule.addTrains(train1)
--- Deployment/objects/train.py
+++ Deployment/objects/train.py
@@ -1,14 +1,23 @@
 class train():
-    def __init__(self, id, name, route) -> None:
+    def __init__(self, id, name, route, timeBetweenStat) -> None:
       self.id = id
       self.name = name
       self.route = route
+      self.times = timeBetweenStat
+      self.length = len(self.route.stations)
 
     def updateRoute(self, newRoute):
       self.route = newRoute
 
     def print(self):
-      self.route.print()
+      for i in range(self.length):
+        print(self.route.stations[i])
+        print("    |     ")
+        print(self.times[i])
+        print("    |     ")
+      print(self.route.stations[self.length]
+      
+      
       
       
   
