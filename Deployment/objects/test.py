efa2d7319b89daa111d43f057a817ad2f0580c82
Abhishek Paul <abhishek.paul0705@gmail.com>
2023-03-25 14:49:30 -0400

    sa

--- Deployment/objects/test.py
+++ Deployment/objects/test.py
@@ -21,9 +21,9 @@ for i in range(len(stations1)):
     route2.addStation(stat2)
     route3.addStation(stat3)
 
-train1 = train(1, "line1Train", route1, [2,3,4,2,2,1,2,6], "5:00")
-train2 = train(2, "line2Train", route2, [2,3,4,2,2,1,2,6], "6:00")
-train3 = train(3, "line3Train", route3, [2,3,4,2,2,1,2,6], "7:00")
+train1 = train("line1Train", route1, [2,3,4,2,2,1,2,6], "5:00")
+train2 = train("line2Train", route2, [8,5,3,2,1,1,1,3], "1:30")
+train3 = train("line3Train", route3, [1,10,2,3,1,9,9,7], "6:45")
 
 mySchedule = trainSchedule(1)
 mySchedule.addTrains(train1)
--- Deployment/objects/train.py
+++ Deployment/objects/train.py
@@ -9,7 +9,7 @@ class train():
       self.stations = self.route.stations
       self.schedule = {}
       self.depart = depart
-      calculateSchedule(self, self.stations[0], self.stations[length-1], depart)
+      self.calculateSchedule(depart)
       
 
     def updateRoute(self, newRoute, timeBetweenStat, name, departTime):
@@ -18,24 +18,19 @@ class train():
       self.route = newRoute
       self.time = timeBetweenStat
       self.length = len(self.newRoute.stations)
-      calculateSchedule(newRoute[0],newRoute[length-1], departTime)
+      self.calculateSchedule(departTime)
       
-    def calculateSchedule(self, startStation, endStation, departTime):
-    
+    def calculateSchedule(self, departTime):
+      start = self.stations[0]
       self.schedule[start] = departTime
       stations = self.route.stations
       
-      for i in range(len(times))
+      for i in range(len(self.times)):
         start = datetime.strptime(departTime, '%H:%M')
-        end = start + timedelta(minutes=time[i])
-        endTimeStr = end_time_obj.strftime('%H:%M')
-        self.schedule[=stations[i+1]] = endTimeStr
+        end = start + timedelta(minutes=self.times[i])
+        endTimeStr = end.strftime('%H:%M')
+        self.schedule[stations[i+1]] = endTimeStr
         departTime = endTimeStr
-
-            
-
-    def calculateRoute(self, station1, station2,departTime):
-      
 
     def print(self):
       for key, value in self.schedule.items():
